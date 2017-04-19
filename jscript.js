aa2630af0cb07f2c7819aa514a47de8cde4aa0c3
Theodoros Kanellopoulos <tkanello@gmail.com>
2017-04-19 22:01:23 +0300

    First Commit

---
+++ jscript.js
@@ -0,0 +1,43 @@
+function myFunction() {
+    //var txt = "";
+
+    if (document.getElementById("pass").validity.patternMismatch) {
+        alert("Not valid pattern");
+    }
+    //document.getElementById("demo").innerHTML = txt;
+}
+
+
+
+
+function checkPassword(str) {
+    var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}$/;
+    //test method for RegEx expressions  
+    return re.test(str);
+}
+
+function checkForm(form) {
+    if (form.username.value == "") {
+        alert("Error: Username cannot be blank!");
+        form.username.focus();
+        return false;
+    }
+    re = /^\w+$/;
+    if (!re.test(form.username.value)) {
+        alert("Error: Username must contain only letters, numbers and underscores!");
+        form.username.focus();
+        return false;
+    }
+    if (form.pwd1.value != "" && form.pwd1.value == form.pwd2.value) {
+        if (!checkPassword(form.pwd1.value)) {
+            alert("The password you have entered is not valid!");
+            form.pwd1.focus();
+            return false;
+        }
+    } else {
+        alert("Error: Please check that you've entered and confirmed your password!");
+        form.pwd1.focus();
+        return false;
+    }
+    return true;
+}
